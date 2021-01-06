ed5f5ffefb8d642261c62921e093481595be14d8
Alan R. Fachini <hi@alfakini.com>
2021-01-06 08:57:03 -0300

    fix(layout): Add essays list to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,55 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
+
+import { rhythm } from "../configs/typography"
 
 import Layout from "../layouts/layout"
 
-const IndexPage = ({ location }) => {
+const PostHeader = ({ data }) => {
+  return (
+    <article key={data.fields.slug}>
+      <header>
+        <h4
+          style={{
+            marginBottom: rhythm(1 / 8),
+          }}
+        >
+          <Link style={{ boxShadow: `none` }} to={`/essays${data.fields.slug}`}>
+            {data.frontmatter.title}
+          </Link>
+        </h4>
+        <small>{data.frontmatter.date}</small>
+      </header>
+    </article>
+  )
+}
+
+const IndexPage = ({ data, location }) => {
+  const essays = data.allMarkdownRemark.edges
 
   return (
     <Layout location={location} title="Blog">
-      {"INDEX"}
+      {essays.map(post => <PostHeader data={post.node} />)}
     </Layout>
   )
 }
 
 export default IndexPage
+
+export const pageQuery = graphql`
+  query {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+          }
+        }
+      }
+    }
+  }
+`
