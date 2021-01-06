4e135c95b866923632d3e4679e5e57e71a15d301
Alan R. Fachini <hi@alfakini.com>
2021-01-06 08:37:11 -0300

    Add books, notes and papers

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,45 @@
-const path = require(`path`)
-const { createFilePath } = require(`gatsby-source-filesystem`)
-
-const essayTemplate = path.resolve(`./src/templates/essay-template.js`)
+const path = require("path")
+const { createFilePath } = require("gatsby-source-filesystem")
+const essayTemplate = path.resolve("./src/templates/essay-template.js")
 
 exports.createPages = async ({ graphql, actions }) => {
   const result = await graphql(`
+    fragment MarkdownRemarkFieldsFragment on MarkdownRemarkEdge {
+      node {
+        fields {
+          authors
+          categories
+          cover
+          date
+          description
+          id
+          images
+          published
+          slug
+          tags
+          title
+        }
+      }
+    }
+
     query {
-        allMarkdownRemark(
+        essays: allMarkdownRemark(
+          filter: {fileAbsolutePath: {regex: "/(essays)/"  }}
           sort: { fields: [frontmatter___date], order: DESC }
           limit: 1000
         ) {
           edges {
-            node {
-              fields {
-                slug
-              }
-              frontmatter {
-                title
-              }
-            }
+            ...MarkdownRemarkFieldsFragment
+          }
+        }
+
+        books: allMarkdownRemark(
+          filter: {fileAbsolutePath: {regex: "/(books)/"  }}
+          sort: { fields: [frontmatter___date], order: DESC }
+          limit: 1000
+        ) {
+          edges {
+            ...MarkdownRemarkFieldsFragment
           }
         }
       
@@ -29,39 +50,60 @@ exports.createPages = async ({ graphql, actions }) => {
     throw result.errors
   }
 
-  const essays = result.data.allMarkdownRemark.edges
-
-  essays.forEach((post, index) => {
+  const essays = result.data.essays.edges
+  
+  essays.forEach((essay, index) => {
     const previous = index === essays.length - 1 ? null : essays[index + 1].node
     const next = index === 0 ? null : essays[index - 1].node
 
-    console.log(`/essays${post.node.fields.slug}`)
+    console.log(`/essays${essay.node.fields.slug}`)
     actions.createPage({
-      path: `/essays${post.node.fields.slug}`,
+      path: `/essays${essay.node.fields.slug}`,
       component: essayTemplate,
       context: {
-        slug: post.node.fields.slug,
+        slug: essay.node.fields.slug,
         previous,
         next,
       },
     })
   })
+
+
+  const books = result.data.books.edges
+
+  books.forEach((book, index) => {
+    const previous = index === books.length - 1 ? null : books[index + 1].node
+    const next = index === 0 ? null : books[index - 1].node
+
+    console.log(`/books${book.node.fields.slug}`)
+    actions.createPage({
+      path: `/books${book.node.fields.slug}`,
+      component: essayTemplate,
+      context: {
+        slug: book.node.fields.slug,
+        previous,
+        next,
+      },
+    })
+  })
+
 }
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
-  if (node.internal.type === `MarkdownRemark`) {
+  if (node.internal.type === "MarkdownRemark") {
 
-    actions.createNodeField({ name: 'id', node, value: node.id })
-    actions.createNodeField({ name: 'type', node, value: node.frontmatter.type || [] })
-    actions.createNodeField({ name: 'title', node, value: node.frontmatter.title })
-    actions.createNodeField({ name: `slug`, node, value: createFilePath({ node, getNode }) })
-    actions.createNodeField({ name: 'description', node, value: node.frontmatter.description })
-    actions.createNodeField({ name: 'authors', node, value: node.frontmatter.authors })
-    actions.createNodeField({ name: 'date', node, value: node.frontmatter.date ? node.frontmatter.date.split(' ')[0] : '' })
-    actions.createNodeField({ name: 'categories', node, value: node.frontmatter.categories })
-    actions.createNodeField({ name: 'tags', node, value: node.frontmatter.tags })
-    actions.createNodeField({ name: 'cover', node, value: node.frontmatter.cover })
-    actions.createNodeField({ name: 'images', node, value: node.frontmatter.images })
-    actions.createNodeField({ name: 'published', node, value: node.frontmatter.published ? true : false })
+    actions.createNodeField({ name: "id", node, value: node.id })
+    actions.createNodeField({ name: "type", node, value: node.frontmatter.type || "" })
+
+    actions.createNodeField({ name: "title", node, value: node.frontmatter.title })
+    actions.createNodeField({ name: "slug", node, value: createFilePath({ node, getNode }) })
+    actions.createNodeField({ name: "description", node, value: node.frontmatter.description })
+    actions.createNodeField({ name: "authors", node, value: node.frontmatter.authors })
+    actions.createNodeField({ name: "date", node, value: node.frontmatter.date ? node.frontmatter.date.split(" ")[0] : "" })
+    actions.createNodeField({ name: "categories", node, value: node.frontmatter.categories })
+    actions.createNodeField({ name: "tags", node, value: node.frontmatter.tags })
+    actions.createNodeField({ name: "cover", node, value: node.frontmatter.cover })
+    actions.createNodeField({ name: "images", node, value: node.frontmatter.images })
+    actions.createNodeField({ name: "published", node, value: node.frontmatter.published })
   }
 }
